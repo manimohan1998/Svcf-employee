e9cd7345422c0220df37369758f5a4c146535fbb
mohan <manimohan.arichandiran@adcltech.com>
2021-07-05 12:22:38 +0530

    envchange

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   Baseurl:'https://emp.sreevisalam.com/SVCF_Service/Service1.svc/',
-    Imageurl:'https://emp.sreevisalam.com'
+    Imageurl:'http://3.7.244.11'
 };
